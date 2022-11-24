76b6ff2a5eeee00d8219a536cfac4e2c1f915a15
priyan2115 <priyankaa2115@gmail.com>
2022-11-23 20:07:56 -0500

    add modifying code

--- queries/Employee_Database_challenge.sql
+++ queries/Employee_Database_challenge.sql
@@ -12,6 +12,20 @@ LEFT JOIN titles as t
 ON (e.emp_no=t.emp_no)
 WHERE(birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 
+--we can get the same output with this code too
+
+SELECT e.emp_no,
+	e.first_name,
+	e.last_name,
+	t.title,
+	t.from_date,
+	t.to_date
+INTO retirement_titles
+FROM employees as e
+INNER JOIN  titles as t
+ON (e.emp_no=t.emp_no)
+WHERE(birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+
 select * from retirement_titles
 
 -- Use Dictinct with Orderby to remove duplicate rows
@@ -52,6 +66,26 @@ FROM employees AS e
 INNER JOIN dept_emp AS de
 ON (e.emp_no=de.emp_no)
 INNER JOIN titles AS t
+ON (e.emp_no=t.emp_no)
+WHERE (de.to_date='9999-01-01')
+AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no;
+
+-- we can get same output through this code too.
+
+SELECT DISTINCT ON (e.emp_no)
+	e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	t.title
+INTO mentorship_eligibilty
+FROM employees AS e
+INNER JOIN dept_emp AS de
+ON (e.emp_no=de.emp_no)
+LEFT OUTER JOIN titles AS t
 ON (e.emp_no=t.emp_no)
 WHERE (de.to_date='9999-01-01')
 AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
